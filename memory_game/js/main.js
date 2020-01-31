fd2be95920246124869b712335fa9501356b19b4
thomdebomb <ohcsthomas@gmail.com>
2020-01-31 17:47:39 +0800

    updated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,19 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPLay = [];
 
+let cardOne = cards[0];
+cardsInPLay.push(cardOne);
 console.log("User flipped " + cardOne);
+
+let cardTwo = cards[2];
+cardsInPLay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPLay.length === 2) {
+	if (cardOne === cardTwo){
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry Try Again!");
+	}
+}
