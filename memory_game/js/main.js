119d2b0289fbec242699fd91eb1038eeb0b202e3
thomdebomb <ohcsthomas@gmail.com>
2020-02-01 14:34:34 +0800

    updated game logic again

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,33 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 const cardsInPlay = [];
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
@@ -14,9 +37,11 @@ function flipCard(cardId) {
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]){
 	console.log("You found a match!");
+	alert("You found a match!");
 	}
 	else {
 	console.log("Sorry Try Again!");
+	alert("Sorry Try Again");
 	}
 }
 
